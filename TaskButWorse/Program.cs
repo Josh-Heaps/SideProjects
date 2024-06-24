c7307c55af8a547dc954082b98d1641d8d979caa
jheaps <jheaps@conservice.com>
2024-06-24 16:07:41 -0600

    Make worse tasks

---
+++ TaskButWorse/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using TaskButWorse;
+WorseTask t = new();
+t.Run(() =>
+{
+    for (int i = 0; i < 10; i++)
+    {
+        Thread.Sleep(10);
+        Console.WriteLine(i);
+    }
+});
+
+await t;
+Console.WriteLine("Next stuff");
---
+++ TaskButWorse/WorseAwaiter.cs
@@ -0,0 +1,28 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace TaskButWorse
+{
+    public class WorseAwaiter : INotifyCompletion
+    {
+        public bool IsCompleted { get; private set; }
+        private readonly WorseTask _task;
+
+        internal WorseAwaiter(WorseTask task)
+        {
+            _task = task;
+        }
+
+        public void OnCompleted(Action continuation)
+        {
+            while (_task.Status != TaskStatus.Completed && _task.Status != TaskStatus.Faulted) { _task.UpdateTaskStatus(); }
+
+            continuation();
+        }
+
+        public void GetResult()
+        {
+            if (_task.Exception is not null)
+                throw _task.Exception;
+        }
+    }
+}
