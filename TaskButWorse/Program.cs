957260807826ac2618c89d922445934f173a4d03
jheaps <jheaps@conservice.com>
2024-06-25 10:04:01 -0600

    Change program.cs example

--- TaskButWorse/Program.cs
+++ TaskButWorse/Program.cs
@@ -1,16 +1,20 @@
 ﻿using WorseTask;
 
-int result = await WorseTask<int>.Run(() =>
+Console.WriteLine("Start task");
+
+WorseTask<int> task = WorseTask<int>.Run(() =>
 {
     int result = 0;
+
     for (int i = 0; i < 10; i++)
     {
         result += i;
         Thread.Sleep(100);
     }
+
     return result;
 });
 
-Console.WriteLine(result);
-Console.WriteLine("Next stuff");
+Console.WriteLine("Other work");
+Console.WriteLine(await task);
 
