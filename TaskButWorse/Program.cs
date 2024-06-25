cfc6cbb9a8d71a0c1605ddeb393dbc7cf5b5459a
jheaps <jheaps@conservice.com>
2024-06-25 17:12:34 -0600

    Update example to be better

--- TaskButWorse/Program.cs
+++ TaskButWorse/Program.cs
@@ -1,20 +1,35 @@
-﻿using WorseTask;
+﻿using TaskButWorse;
 
-Console.WriteLine("Start task");
-
-WorseTask<int> task = WorseTask<int>.Run(() =>
+WorseTask<int> additionTask = WorseTask<int>.Run(() =>
 {
     int result = 0;
 
     for (int i = 0; i < 10; i++)
     {
         result += i;
-        Thread.Sleep(100);
+        Thread.Sleep(2);
+    }
+
+    return result;
+});
+
+WorseTask<int> multiplicationTask = WorseTask<int>.Run(() =>
+{
+    int result = 1;
+
+    for (int i = 1; i <= 10 ; i++)
+    {
+        result *= i;
+        Thread.Sleep(10);
     }
 
     return result;
 });
 
 Console.WriteLine("Other work");
-Console.WriteLine(await task);
+var result = await WorseTask<int>.WhenAny(additionTask, multiplicationTask);
+var slowResult = result == additionTask ? multiplicationTask : additionTask;
+Console.WriteLine("Multiply finished first: " + (result == multiplicationTask).ToString());
+Console.WriteLine(await result!);
+Console.WriteLine(await slowResult);
 
